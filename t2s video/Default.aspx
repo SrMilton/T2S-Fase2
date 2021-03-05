9aa42684ace9c08cf697e5597c8d1550fc30d80b
Milton Ramos <milton_da_baik@hotmail.com>
2021-03-05 19:31:52 -0300

    Versão Final

---
+++ t2s video/Default.aspx
@@ -0,0 +1,164 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="t2s_video.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link type="text/css"rel="stylesheet" href="Content/bootstrap.css"/>
+</head>
+    <script src="Scripts/jquery-1.9.0.min.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        #REGISTRAR CONTÊINER
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Cliente:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtCliente" CssClass="form-control" placeholder="Nome" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Número do Contêiner:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtConteinerNum" CssClass="form-control" placeholder="TEST1234567" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Tipo:
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="listTipo" runat="server" Width="128px">
+                          <asp:ListItem Selected="True" Value="20"> 20 </asp:ListItem>
+                          <asp:ListItem Value="40"> 40 </asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Status:
+                    </td>
+                    <td>
+                         <asp:DropDownList ID="listStatus" runat="server" Width="128px">
+                          <asp:ListItem Selected="True" Value="Cheio"> Cheio </asp:ListItem>
+                          <asp:ListItem Value="Vazio"> Vazio </asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Categoria:
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="listCategoria" runat="server" Width="128px">
+                          <asp:ListItem Selected="True" Value="Importação"> Importação </asp:ListItem>
+                          <asp:ListItem Value="Exportação"> Exportação </asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="btnAdd" runat="server" Text="Adicionar" CssClass="btn btn-primary" Width="80px" OnClick="btnAdd_Click" />
+                        <asp:Button ID="btnUpdate" runat="server" Text="Atualizar" CssClass="btn btn-primary" Width="80px" OnClick="btnUpdate_Click" />
+                        <asp:Button ID="btnDelete" runat="server" Text="Deletar" CssClass="btn btn-primary" Width="80px" OnClick="btnDelete_Click" />  
+                    </td>
+                    <td>
+                        <asp:Label ID="labelAdd" runat="server" Text=""></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        ----------------------------------------------
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #REGISTRAR MOVIMENTAÇÃO
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Número do Contêiner:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtConteinerMov" CssClass="form-control" placeholder="TEST1234567" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Tipo da movimentação:
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="listMovimentacao" runat="server" Width="128px">
+                          <asp:ListItem Selected="True" Value="Embarque"> Embarque </asp:ListItem>
+                          <asp:ListItem Value="Descarga"> Descarga </asp:ListItem>
+                          <asp:ListItem Value="Gate In"> Gate In </asp:ListItem>
+                          <asp:ListItem Value="Gate Out"> Gate Out </asp:ListItem>
+                          <asp:ListItem Value="Posicionamento Pilha"> Posicionamento Pilha </asp:ListItem>
+                          <asp:ListItem Value="Pesagem"> Pesagem </asp:ListItem>
+                          <asp:ListItem Value="Scanner"> Scanner </asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Início:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtinicio" CssClass="form-control" placeholder="24/05/2020 22:38" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Fim:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtFim" CssClass="form-control" placeholder="25/05/2020 18:46" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="btnRegistrarMovimentacao" runat="server" Text="Registrar" CssClass="btn btn-primary" Width="80px" OnClick="btnRegistrarMovimentacao_Click" />
+                        <asp:Button ID="btnUpdateMovimentacao" runat="server" Text="Atualizar" CssClass="btn btn-primary" Width="80px" OnClick="btnUpdateMovimentacao_Click" />
+                        <asp:Button ID="btnDeleteMovimentacao" runat="server" Text="Deletar" CssClass="btn btn-primary" Width="80px" OnClick="btnDeleteMovimentacao_Click" />
+                    </td>
+                    <td>
+                        <asp:Label ID="labelMovimentacao" runat="server" Text=""></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="btnRelatorio" runat="server" Text="Gerar Relatório" CssClass="btn btn-primary" OnClick="btnRelatorio_Click" />
+                    </td>
+                </tr>
+            </table>
+            <table>
+            <tr>
+                <td>
+            
+                    <asp:GridView ID="GridView1" runat="server" CssClass="table table-striped"></asp:GridView>
+                 </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:GridView ID="GridView2" runat="server" CssClass="table table-striped"></asp:GridView>
+                    </td>
+                </tr>
+            
+                
+             </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ t2s video/Default.aspx.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace t2s_video
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        SqlConnection connection = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter sda = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            connection.ConnectionString = "Data Source=DESKTOP-DJ8IC52\\SQLEXPRESS;Initial Catalog=dbvideo;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            connection.Open();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtCliente.Text == "" || txtConteinerNum.Text == "")
+            {
+                labelAdd.Text = "Por favor preencha todas as colunas.";
+                labelAdd.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                if (Regex.IsMatch(txtConteinerNum.Text, @"^[a-zA-Z]{4}[0-9]{7}$"))
+                {
+
+                    dt = new DataTable(); //Codigo pra verificar se o conteiner existe no sistema
+                    cmd.CommandText = "SELECT 1 FROM tabelaConteiner WHERE Numero = '" + txtConteinerNum.Text.ToString() + "' ";
+                    cmd.Connection = connection;
+                    cmd.ExecuteNonQuery();
+                    SqlDataReader myreader;
+                    myreader = cmd.ExecuteReader();
+
+                    if (myreader.Read().ToString() == "False") //if not exists
+                    {
+                        myreader.Close();
+                        dt = new DataTable();
+                        cmd.CommandText = "Insert into tabelaConteiner (Nome,Numero,Tipo,Status,Categoria)values('" + txtCliente.Text.ToString() + "','" + txtConteinerNum.Text.ToString() + "','" + listTipo.Text.ToString() + "', '" + listStatus.Text.ToString() + "','" + listCategoria.Text.ToString() + "')";
+                        cmd.Connection = connection;
+                        cmd.ExecuteNonQuery();
+                        labelAdd.Text = "Contêiner adicionado com sucesso.";
+                        labelAdd.ForeColor = System.Drawing.Color.Green;
+                    }
+                    else
+                    {
+                        labelAdd.Text = "Numeração já consta no sistema.";
+                        labelAdd.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+                else
+                {
+                    labelAdd.Text = "Numeração do contêiner deve conter 4 letras e 7 numeros. Ex:ABCD1234567";
+                    labelAdd.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+
+        public void DataShow()
+        {
+            ds = new DataSet();
+            //cmd.CommandText = "Select * From tabelaCliente ORDER BY Cliente";
+            cmd.CommandText = "Select tabelaConteiner.Nome,tabelaConteiner.Numero,tabelaConteiner.Status,tabelaMovimentacao.Tipo,tabelaMovimentacao.Inicio,tabelaMovimentacao.Fim From tabelaConteiner FULL JOIN tabelaMovimentacao ON tabelaConteiner.Numero=tabelaMovimentacao.Numero ORDER BY Nome,Tipo";
+            cmd.Connection = connection;
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+            ds = new DataSet();
+            cmd.CommandText = "Select Categoria, count(*) as Total FROM tabelaConteiner GROUP BY Categoria"; //Segunda tabela
+            cmd.Connection = connection;
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+            GridView2.DataSource = ds;
+            GridView2.DataBind();
+
+            connection.Close();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtCliente.Text == "" || txtConteinerNum.Text == "")
+            {
+                labelAdd.Text = "Por favor preencha todas as colunas.";
+                labelAdd.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                dt = new DataTable(); //Codigo pra verificar se o conteiner existe no sistema
+                cmd.CommandText = "SELECT 1 FROM tabelaConteiner WHERE Numero = '" + txtConteinerNum.Text.ToString() + "' ";
+                cmd.Connection = connection;
+                cmd.ExecuteNonQuery();
+                SqlDataReader myreader;
+                myreader = cmd.ExecuteReader();
+
+                if (myreader.Read().ToString() == "True") //if exists
+                {
+                    myreader.Close();
+                    dt = new DataTable();
+                    cmd.CommandText = "Update tabelaConteiner set Nome='" + txtCliente.Text.ToString() + "',Numero='" + txtConteinerNum.Text.ToString() + "',Tipo='" + listTipo.Text.ToString() + "',Status='" + listStatus.Text.ToString() + "',Categoria='" + listCategoria.Text.ToString() + "' Where Numero='" + txtConteinerNum.Text.ToString() + "'";
+                    cmd.Connection = connection;
+                    cmd.ExecuteNonQuery();
+                    labelAdd.Text = "Contêiner atualizado com sucesso.";
+                    labelAdd.ForeColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    labelAdd.Text = "Numeração não encontrada no sistema.";
+                    labelAdd.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtConteinerNum.Text == "")
+            {
+                labelAdd.Text = "Por favor digite a numeração do contêiner.";
+                labelAdd.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                dt = new DataTable(); //Codigo pra verificar se o conteiner existe no sistema
+                cmd.CommandText = "SELECT 1 FROM tabelaConteiner WHERE Numero = '" + txtConteinerNum.Text.ToString() + "' ";
+                cmd.Connection = connection;
+                cmd.ExecuteNonQuery();
+                SqlDataReader myreader;
+                myreader = cmd.ExecuteReader();
+
+                if (myreader.Read().ToString() == "True") //if exists
+                {
+                    myreader.Close();
+                    dt = new DataTable();
+                    cmd.CommandText = "Delete from tabelaConteiner where Numero='" + txtConteinerNum.Text.ToString() + "'";
+                    cmd.Connection = connection;
+                    cmd.ExecuteNonQuery();
+                    labelAdd.Text = "Contêiner deletado com sucesso.";
+                    labelAdd.ForeColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    labelAdd.Text = "Numeração não encontrada no sistema.";
+                    labelAdd.ForeColor = System.Drawing.Color.Red;
+                }
+            
+        }
+    }
+
+        protected void btnRegistrarMovimentacao_Click(object sender, EventArgs e)
+        {
+            if (txtConteinerMov.Text == "")
+            {
+                labelMovimentacao.Text = "Por favor digite a numeração do contêiner.";
+                labelMovimentacao.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                dt = new DataTable(); //Codigo pra verificar se o conteiner existe no sistema
+                cmd.CommandText = "SELECT 1 FROM tabelaConteiner WHERE Numero = '" + txtConteinerMov.Text.ToString() + "' ";
+                cmd.Connection = connection;
+                cmd.ExecuteNonQuery();
+                SqlDataReader myreader;
+                myreader = cmd.ExecuteReader();
+
+                if (myreader.Read().ToString() == "True") //if exists
+                {
+                    myreader.Close();
+                   
+
+                    dt = new DataTable();
+                    cmd.CommandText = "Insert into tabelaMovimentacao (Inicio,Fim,Numero,Tipo)values('" + txtinicio.Text.ToString() + "','" + txtFim.Text.ToString() + "','" + txtConteinerMov.Text.ToString() + "','" + listMovimentacao.Text.ToString() + "')";
+                    cmd.Connection = connection;
+                    cmd.ExecuteNonQuery();
+                    labelMovimentacao.Text = "Movimentação registrada no sistema com sucesso.";
+                    labelMovimentacao.ForeColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    labelMovimentacao.Text = "Numeração não encontrada no sistema.";
+                    labelMovimentacao.ForeColor = System.Drawing.Color.Red;
+                }
+
+            }
+        }
+
+        protected void btnUpdateMovimentacao_Click(object sender, EventArgs e)
+        {
+            if (txtConteinerMov.Text == "" || txtinicio.Text == "" || txtFim.Text == "")
+            {
+                labelMovimentacao.Text = "Por favor preencha todos os campos.";
+                labelMovimentacao.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                dt = new DataTable(); //Codigo pra verificar se o conteiner existe no sistema
+                cmd.CommandText = "SELECT 1 FROM tabelaMovimentacao WHERE Numero = '" + txtConteinerMov.Text.ToString() + "' AND Inicio='" + txtinicio.Text.ToString() + "' AND Fim='" + txtFim.Text.ToString() + "'";
+                cmd.Connection = connection;
+                cmd.ExecuteNonQuery();
+                SqlDataReader myreader;
+                myreader = cmd.ExecuteReader();
+
+                if (myreader.Read().ToString() == "True") //if exists
+                {
+                    myreader.Close();
+
+                    dt = new DataTable();
+                    cmd.CommandText = "Update tabelaMovimentacao set Inicio='" + txtinicio.Text.ToString() + "',Fim='" + txtFim.Text.ToString() + "',Tipo='" + listMovimentacao.Text.ToString() + "' Where Numero='" + txtConteinerMov.Text.ToString() + "' AND Inicio='" + txtinicio.Text.ToString() + "' AND Fim='" + txtFim.Text.ToString() + "'";
+                    cmd.Connection = connection;
+                    cmd.ExecuteNonQuery();
+                    labelMovimentacao.Text = "Movimentação atualizada com sucesso!";
+                    labelMovimentacao.ForeColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    labelMovimentacao.Text = "Movimentação não encontrado no sistema.";
+                    labelMovimentacao.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+
+        protected void btnDeleteMovimentacao_Click(object sender, EventArgs e)
+        {
+
+            if (txtConteinerMov.Text == "" || txtinicio.Text == "" || txtFim.Text == "")
+            {
+                labelMovimentacao.Text = "Por favor preencha todos os campos.";
+                labelMovimentacao.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                dt = new DataTable(); //Codigo pra verificar se o conteiner existe no sistema
+                cmd.CommandText = "SELECT 1 FROM tabelaMovimentacao WHERE Numero = '" + txtConteinerMov.Text.ToString() + "' AND Inicio= '" + txtinicio.Text.ToString() + "' AND Fim= '" + txtFim.Text.ToString() + "'";
+                cmd.Connection = connection;
+                cmd.ExecuteNonQuery();
+                SqlDataReader myreader;
+                myreader = cmd.ExecuteReader();
+
+                if (myreader.Read().ToString() == "True") //if exists
+                {
+                    myreader.Close();
+
+                    dt = new DataTable();
+                    cmd.CommandText = "Delete from tabelaMovimentacao Where Numero='" + txtConteinerMov.Text.ToString() + "' AND Inicio= '" + txtinicio.Text.ToString() + "' AND Fim= '" + txtFim.Text.ToString() + "'";
+                    cmd.Connection = connection;
+                    cmd.ExecuteNonQuery();
+                    labelMovimentacao.Text = "Movimentação removida com sucesso.";
+                    labelMovimentacao.ForeColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    labelMovimentacao.Text = "Movimentação não encontrado no sistema.";
+                    labelMovimentacao.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+
+        protected void btnRelatorio_Click(object sender, EventArgs e)
+        {
+            DataShow();
+        }
+    }
+}
